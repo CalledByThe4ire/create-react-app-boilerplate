704a2ba9d3a5ccabd39dbc242f03ea19db6ef554
Andrey Karamnoff <karamnoff@gmail.com>
2020-08-07 15:27:18 +0300

    initial commit

---
+++ Makefile
@@ -0,0 +1,19 @@
+install:
+	npm install
+
+build:
+	npm run build
+
+develop:
+	npm start
+
+stylelint:
+	npm run stylelint
+
+eslint:
+	npm run eslint
+
+test:
+	npm test
+
+.PHONY: test
---
+++ src/components/app/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import classnames from 'classnames';
+import styles from './app.module.scss';
+
+function App() {
+    return <div className={classnames(styles.App)}></div>;
+}
+
+export default App;
